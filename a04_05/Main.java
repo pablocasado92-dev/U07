edca1fab1eb4e30e058c0836e612972dd4168958
pcc <pablo.casado92@gmail.com>
2026-03-17 10:31:09 +0100

    Modifico el Main para añadir un bucle que pide cuantas notas quieres añadir

--- a04_05/Main.java
+++ a04_05/Main.java
@@ -1,12 +1,25 @@
 package a04_05;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Piano piano1 = new Piano();
-        for (Nota nota : Nota.values()) { // Bucle para recorrer todas las notas en el enum Nota
+
+        // Solicitar al usuario cuántas notas desea añadir
+        System.out.print("¿Cuántas notas quieres añadir?: ");
+        int cantidadNotas = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < cantidadNotas; i++) {
+            System.out.print("Introduce la nota " + (i + 1) + ": ");
+            String notaStr = sc.nextLine();
+            Nota nota = Nota.valueOf(notaStr.toUpperCase());
             piano1.add(nota);
         }
-        piano1.interpretar();
-    }
 
+        piano1.interpretar();
+        sc.close();
+    }
 }
